00727da72d841a292b2c25f0c96dda7e57ff2b20
scsi123 <scsi1234@yandex.ru>
2022-11-03 02:47:31 +0300

    Первый API тест
    - импорт классов RestAssured;
    - создан GET запрос на адрес api;
    - возврат результата с помощью restassured;
    - в переменной response храниться информация об ответе на запрос;
    - распечатка текста ответа в json формате с помощью функции prettyPrint

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,9 +1,14 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
 
   @Test
   public void testHelloWorld(){
-    System.out.println("Hello World");
+    Response response = RestAssured
+            .get("https://playground.learnqa.ru/api/hello")
+            .andReturn();
+    response.prettyPrint();
   }
 }
