cce85cd1840e53396cdd51fafd1c5d28ea9e60ea
scsi123 <scsi1234@yandex.ru>
2022-11-03 03:24:02 +0300

    GET-запрос по адресу: https://playground.learnqa.ru/api/get_text

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -6,6 +6,9 @@ public class HelloWorldTest {
 
   @Test
   public void testHelloWorld() {
-    System.out.println("Hello from Anatoly");
+    Response response = RestAssured
+            .get("https://playground.learnqa.ru/api/get_text")
+            .andReturn();
+    response.prettyPrint();
   }
 }
