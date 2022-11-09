08a345b3dcd1547c036cfc7930a51ad892c0ca78
scsi123 <scsi1234@yandex.ru>
2022-11-09 23:36:09 +0300

    Тест посылка запроса и распечатка его

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,28 +1,16 @@
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class HelloWorldTest {
 
   @Test
   public void testRestAssured() {
-    Map<String, String> params = new HashMap<>();
-    params.put("name", "John");
+    Response response = RestAssured
+            .get("https://playground.learnqa.ru/api/check_type")
+            .andReturn();
 
-    JsonPath response = RestAssured
-            .given()
-            .queryParams(params)
-            .get("https://playground.learnqa.ru/api/hello")
-            .jsonPath();
-
-    String name = response.get("answer2");
-    if (name == null) {
-      System.out.println("The key 'answer2' is absent");
-    } else {
-      System.out.println(name);
-    }
+    response.prettyPrint();
   }
 }
