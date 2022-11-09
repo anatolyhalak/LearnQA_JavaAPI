2dcd105aa570345d769980e711c9c6071fa0ea95
scsi123 <scsi1234@yandex.ru>
2022-11-09 23:17:38 +0300

    Код первого запроса с передачей параметров

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.Test;
 public class HelloWorldTest {
 
   @Test
-  public void testHelloWorld() {
+  public void testRestAssured() {
     Response response = RestAssured
-            .get("https://playground.learnqa.ru/api/get_text")
+            .given()
+            .queryParam("name", "John")
+            .get("https://playground.learnqa.ru/api/hello")
             .andReturn();
     response.prettyPrint();
   }
