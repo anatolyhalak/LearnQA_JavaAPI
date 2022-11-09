9c5059a1221d92c35f5513646c44653ad8c6f453
scsi123 <scsi1234@yandex.ru>
2022-11-09 23:39:08 +0300

    В списке get параметров появились нужные параметры из кода

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -8,9 +8,12 @@ public class HelloWorldTest {
   @Test
   public void testRestAssured() {
     Response response = RestAssured
+            .given()
+            .queryParam("param1", "value1")
+            .queryParam("param2", "value2")
             .get("https://playground.learnqa.ru/api/check_type")
             .andReturn();
 
-    response.prettyPrint();
+    response.print();
   }
 }
