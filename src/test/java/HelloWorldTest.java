67ef5f7f0d7ea378c9ae9d977b7a01294bad5d23
scsi123 <scsi1234@yandex.ru>
2022-11-09 23:51:24 +0300

    Отправка post запросов с различным набором параметров

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,15 +1,21 @@
 import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HelloWorldTest {
 
   @Test
   public void testRestAssured() {
+    Map<String,Object> body = new HashMap<>();
+    body.put("param1", "value1");
+    body.put("param2", "value2");
+
     Response response = RestAssured
             .given()
-            .body("{\"param1\":\"value1\",\"param2\":\"value2\"}")
+            .body(body)
             .post("https://playground.learnqa.ru/api/check_type")
             .andReturn();
 
