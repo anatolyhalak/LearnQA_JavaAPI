ef146136f55cf645cd01d0728600fa7894d34e96
scsi123 <scsi1234@yandex.ru>
2022-11-09 23:22:12 +0300

    Запрос с помощью queryParams

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -2,13 +2,19 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HelloWorldTest {
 
   @Test
   public void testRestAssured() {
+    Map<String, String> params = new HashMap<>();
+    params.put("name", "John");
+
     Response response = RestAssured
             .given()
-            .queryParam("name", "John")
+            .queryParams(params)
             .get("https://playground.learnqa.ru/api/hello")
             .andReturn();
     response.prettyPrint();
