7ff1a391e12e70959c7765562a863010f6b312af
scsi123 <scsi1234@yandex.ru>
2022-11-21 00:20:37 +0300

    первые тесты assertEquals

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -5,20 +5,24 @@ import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class HelloWorldTest {
 
   @Test
-  public void testRestAssured() {
+  public void testFor200() {
     Response response = RestAssured
-            .given()
-            .redirects()
-            .follow(true)
-            .when()
-            .get("https://playground.learnqa.ru/api/get_303")
+            .get("https://playground.learnqa.ru/api/map")
             .andReturn();
-
-    int statusCode = response.getStatusCode();
-    System.out.println(statusCode);
+    assertEquals(200, response.statusCode(), "Unexpected status code");
+  }
+  @Test
+  public void testFor404() {
+    Response response = RestAssured
+            .get("https://playground.learnqa.ru/api/map2")
+            .andReturn();
+    assertEquals(404, response.statusCode(), "Unexpected status code");
   }
 }
 
